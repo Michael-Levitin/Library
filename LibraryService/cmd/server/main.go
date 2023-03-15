3418623334e69e7cdb53bd00af3a3c1fdf974e34
Michael Levitin <stgriard@yandex.ru>
2023-03-15 20:48:49 +0300

    db connect

--- LibraryService/cmd/server/main.go
+++ LibraryService/cmd/server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
-	"net"
-	"os"
+	_ "github.com/go-sql-driver/mysql"
 
 	libServer "github.com/Michael-Levitin/Library/LibraryService/internal/library"
 	pb "github.com/Michael-Levitin/Library/LibraryService/proto"
 	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
 )
 
 var (
@@ -17,6 +19,14 @@ var (
 )
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/library")
+	if err != nil {
+		log.Println("error connecting to database: ", err)
+		os.Exit(1)
+	}
+	log.Println("connected to database")
+	defer db.Close()
+
 	addr := fmt.Sprintf("%s:%s", host, port)
 	lis, err := net.Listen("tcp", addr)
 
