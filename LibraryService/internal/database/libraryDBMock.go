259235f664831ad13fa18cad57eb7b405f2d2345
Michael Levitin <stgriard@yandex.ru>
2023-03-19 21:43:49 +0300

    switched to named fields in structs

--- LibraryService/internal/database/libraryDBMock.go
+++ LibraryService/internal/database/libraryDBMock.go
@@ -15,10 +15,10 @@ func NewLibraryDBMock() *libraryDBMock {
 func (l libraryDBMock) GetAuthorLike(ctx context.Context, name string) (*[]ob.BookDB, error) {
 	if name == "Man" {
 		return &[]ob.BookDB{
-			{"Alexander Belyaev", "Amphibian Man"},
-			{"Alexander Pushkin", "The Bronze Horseman"},
-			{"Mikhail Sholokhov", "The fate of man"},
-			{"Ernest Hemingway", "The Old Man and the Sea"},
+			{Name: "Alexander Belyaev", Title: "Amphibian Man"},
+			{Name: "Alexander Pushkin", Title: "The Bronze Horseman"},
+			{Name: "Mikhail Sholokhov", Title: "The fate of man"},
+			{Name: "Ernest Hemingway", Title: "The Old Man and the Sea"},
 		}, nil
 	}
 	return &[]ob.BookDB{}, nil
@@ -30,7 +30,7 @@ func (l libraryDBMock) GetAuthorExact(ctx context.Context, title string) (*[]ob.
 	}
 	if title == "Amphibian Man" { // а такое название есть - возвращаем
 		return &[]ob.BookDB{
-			{"Alexander Belyaev", "Amphibian Man"},
+			{Name: "Alexander Belyaev", Title: "Amphibian Man"},
 		}, nil
 	}
 	return nil, ob.SomeError
@@ -39,25 +39,25 @@ func (l libraryDBMock) GetAuthorExact(ctx context.Context, title string) (*[]ob.
 func (l libraryDBMock) GetTitleLike(ctx context.Context, author string) (*[]ob.BookDB, error) {
 	if author == "Chehov" { // "ищем" и возвращаем по частичному значение
 		return &[]ob.BookDB{
-			{"Anton Chekhov", "The Cherry Orchard"},
-			{"Anton Chekhov", "Hunting Drama"},
-			{"Anton Chekhov", "Uncle Vanya"},
-			{"Anton Chekhov", "Ward No. 6"},
-			{"Anton Chekhov", "Stories"},
-			{"Anton Chekhov", "Three Sisters"},
-			{"Anton Chekhov", "Seagull"}}, nil
+			{Name: "Anton Chekhov", Title: "The Cherry Orchard"},
+			{Name: "Anton Chekhov", Title: "Hunting Drama"},
+			{Name: "Anton Chekhov", Title: "Uncle Vanya"},
+			{Name: "Anton Chekhov", Title: "Ward No. 6"},
+			{Name: "Anton Chekhov", Title: "Stories"},
+			{Name: "Anton Chekhov", Title: "Three Sisters"},
+			{Name: "Anton Chekhov", Title: "Seagull"}}, nil
 	}
 
 	if author == "Tolstoy" { // "ищем" и возвращаем по частичному значение
 		return &[]ob.BookDB{
-			{"Alexey Tolstoy", "Peter the Great"},
-			{"Alexey Tolstoy", "Going through the throes"},
-			{"Lev Tolstoy", "Anna Karenina"},
-			{"Lev Tolstoy", "War and Peace"},
-			{"Lev Tolstoy", "Resurrection"},
-			{"Lev Tolstoy", "Childhood. Adolescence. Youth"},
-			{"Lev Tolstoy", "Prisoner of the Caucasus"},
-			{"Lev Tolstoy", "Hadji Murad"}}, nil
+			{Name: "Alexey Tolstoy", Title: "Peter the Great"},
+			{Name: "Alexey Tolstoy", Title: "Going through the throes"},
+			{Name: "Lev Tolstoy", Title: "Anna Karenina"},
+			{Name: "Lev Tolstoy", Title: "War and Peace"},
+			{Name: "Lev Tolstoy", Title: "Resurrection"},
+			{Name: "Lev Tolstoy", Title: "Childhood. Adolescence. Youth"},
+			{Name: "Lev Tolstoy", Title: "Prisoner of the Caucasus"},
+			{Name: "Lev Tolstoy", Title: "Hadji Murad"}}, nil
 	}
 
 	return &[]ob.BookDB{}, nil
@@ -70,7 +70,7 @@ func (l libraryDBMock) GetTitleExact(ctx context.Context, author string) (*[]ob.
 
 	if author == "Alexander Belyaev" { // а такое значение есть
 		return &[]ob.BookDB{
-			{"Alexander Belyaev", "Amphibian Man"},
+			{Name: "Alexander Belyaev", Title: "Amphibian Man"},
 		}, nil
 	}
 
@@ -80,13 +80,13 @@ func (l libraryDBMock) GetTitleExact(ctx context.Context, author string) (*[]ob.
 
 	if author == "Anton Chekhov" { // а такое значение есть
 		return &[]ob.BookDB{
-			{"Anton Chekhov", "The Cherry Orchard"},
-			{"Anton Chekhov", "Hunting Drama"},
-			{"Anton Chekhov", "Uncle Vanya"},
-			{"Anton Chekhov", "Ward No. 6"},
-			{"Anton Chekhov", "Stories"},
-			{"Anton Chekhov", "Three Sisters"},
-			{"Anton Chekhov", "Seagull"},
+			{Name: "Anton Chekhov", Title: "The Cherry Orchard"},
+			{Name: "Anton Chekhov", Title: "Hunting Drama"},
+			{Name: "Anton Chekhov", Title: "Uncle Vanya"},
+			{Name: "Anton Chekhov", Title: "Ward No. 6"},
+			{Name: "Anton Chekhov", Title: "Stories"},
+			{Name: "Anton Chekhov", Title: "Three Sisters"},
+			{Name: "Anton Chekhov", Title: "Seagull"},
 		}, nil
 	}
 
