45b1bc1f31e93a4c1c4c9197d20683bfbaa7c28d
Michael Levitin <stgriard@yandex.ru>
2023-03-22 15:42:27 +0300

    db update with another table

--- LibraryData/sql-scripts/CreateTable.sql
+++ LibraryData/sql-scripts/CreateTable.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `library` CHARACTER SET = 'utf8mb4' COLLATE = 'utf8mb4_general_ci';
 USE `library`;
 
+DROP TABLE if exists `books_authors`;
 DROP TABLE if exists `books`;
 DROP TABLE if exists `authors`;
 
@@ -17,4 +18,13 @@ CREATE TABLE `books`
     `author_id` INT(11)      NOT NULL,
     `title`     VARCHAR(255) NOT NULL,
     FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
+);
+
+CREATE TABLE `books_authors`
+(
+    `book_id`   INT(11) NOT NULL,
+    `author_id` INT(11) NOT NULL,
+    PRIMARY KEY (`book_id`, `author_id`),
+    FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`) ON DELETE CASCADE
 );
